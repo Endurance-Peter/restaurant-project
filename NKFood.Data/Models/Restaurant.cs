69140bbc018618c35ef5b74459c0cf9b50586f00
Endurance-Peter <endurance.peter@cyphercrescent.com>
2022-11-22 11:19:18 +0100

    Add project files.

---
+++ NKFood.Data/Models/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NKFood.Data.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public CuisineType CuisineType { get; set; }
+    }
+
+    public enum CuisineType
+    {
+        None, 
+        Italian,
+        Indian,
+        French
+    }
+
+   
+}
---
+++ NKFood.Data/Services/IRestaurantData.cs
@@ -0,0 +1,32 @@
+﻿using NKFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NKFood.Data.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class INMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public INMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{ Id =1, Name="Scottish Pizza", CuisineType= CuisineType.French},
+                new Restaurant{ Id =2, Name="Mango Grove", CuisineType= CuisineType.Italian},
+                new Restaurant{ Id =3, Name="Tersiguels", CuisineType= CuisineType.Indian},
+                new Restaurant{ Id =4, Name="Ken Pizza", CuisineType= CuisineType.French}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(x => x.Name);
+        }
+    }
+}
